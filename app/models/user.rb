3e767270bbdaf63ca38ed1fb0a8ba6aeaa25a4d1
xbalu <bmaziarz1993@gmail.com>
2017-09-17 22:55:24 +0200

    Add sign in, create new user, user information to right nav bar, customize devise forms outlook

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :nickname, presence: true, length: { minimum: 3 }, uniqueness: true
 end
